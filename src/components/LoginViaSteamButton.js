fef8a4061b7af4769ec20fd24a7ee6a2f27a07b1
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-11-30 22:09:47 +0000

    Extracted Function For Readability

--- src/components/LoginViaSteamButton.js
+++ src/components/LoginViaSteamButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as qs from 'query-string';
 
 export default class LoginViaSteamButton extends React.Component {
     state = {
@@ -6,17 +7,23 @@ export default class LoginViaSteamButton extends React.Component {
     };
 
     componentDidMount() {
+        const url = qs.parse(window.location.search);
+        console.log(url);
+        this.fetchSteamOpenIdUrl();
+    }
+
+    fetchSteamOpenIdUrl() {
         fetch('http://localhost:8080/api/steam/login', {method: 'get'})
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            if (!response.valid) throw Error(response.statusText);
-            this._asyncRequest = null;
-            this.setState({steamRedirectUrl: response.value.toString()});
-        })
-        .catch(error => {
-            console.log(error);
-        });
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                if (!response.valid) throw Error(response.statusText);
+                this._asyncRequest = null;
+                this.setState({steamRedirectUrl: response.value.toString()});
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     componentWillUnmount() {
